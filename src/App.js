702ecadc4f8ff51bdb72b3d2173f6e2873f83229
Yumiko Mannarelli <yumikomannarelli@gmail.com>
2021-02-09 17:00:52 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import Slide from './Slide.js'
 
 function App() {
+  const data = [ {
+    id: 0,
+    slideHeader: 'What is Latana?',
+    slideSubHeaders: ['How does Latana Work?', 'Why choose Latana', 'What does Latana cost?'],
+    img: './slide0.png',
+    redirect: 'url'
+  },{
+    id: 1,
+    slideHeader: 'Getting Started',
+    slideSubHeaders: ['What is MRP?', 'How do you gather data?', 'What is a margin of error?'],
+    img: './slide1.png',
+    redirect: 'url'
+  },
+  {
+    id: 2,
+    slideHeader: 'Using Latana',
+    slideSubHeaders: ['How to use Latana?', 'What is the Brand Funnel?', 'How to interpret Brand Tracking Data?'],
+    img:'./slide2.png',
+    redirect: 'url'
+  },
+  {
+    id: 3,
+    slideHeader: 'Data Collection & Methodology',
+    slideSubHeaders: ['What is MRP?', 'How do you gather data?', 'What is a margin of error?'],
+    img: './slide3.png',
+    redirect: 'url'
+  },
+  {
+    id: 4,
+    slideHeader: 'Sucess Strategies',
+    slideSubHeaders: ['What is MRP?', 'How do you gather data?', 'What is a margin of error?'],
+    img: './slide3.png',
+    redirect: 'url'
+  }]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-Outer">
+    <div className="App-Inner">
+      <div className='Header'>Latana Knowledge Center</div>
+      <div className="Header-Subtext">Everything you need to know about the best brand tracking solution in the world.</div>
+      <div className="Slider">
+        {data.map((val, index) => {
+          if(index === val.id) {
+            return <Slide val={val} key={index}/>
+          }
+        })}
+      </div>
+    </div>
     </div>
   );
 }
---
+++ src/Slide.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+
+function Slide(props) {
+  console.log(props)
+  const {id, img, slideHeader, slideSubHeaders} = props.val
+
+  return (
+    <div className="Slide">
+      <img src={img} />
+      <div className="Slide-Header">{slideHeader}</div>
+      <div className="Slide-Subheaders">
+        <div className="Slide-Subheader-Normal">{slideSubHeaders ? slideSubHeaders[0]: <></>}</div>
+        <div className="Slide-Subheader-Normal">{slideSubHeaders ? slideSubHeaders[1]: <></>}</div>
+        <div className="Slide-Subheader-Normal">{slideSubHeaders ? slideSubHeaders[2] :<></>}</div>
+      </div>
+      <button>Learn More</button>
+    </div>
+  );
+}
+
+export default Slide;
